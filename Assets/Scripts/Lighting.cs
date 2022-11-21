ab4b3e892f87e741cf7a4611ae0bb97867ddd912
aseay4 <86761894+aseay4@users.noreply.github.com>
2022-11-20 23:03:36 -0500

    Light fix, boss positioned

--- Assets/Scripts/Lighting.cs
+++ Assets/Scripts/Lighting.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Lighting : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-
+        GetComponent<RenderSettings>();
     }
 
     // Update is called once per frame
